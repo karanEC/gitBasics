e5c5b4c0f51c9c809816a6139508785058c36d76
karanEC <32287405+karanEC@users.noreply.github.com>
2017-10-11 12:28:34 +0530

    Comments for Person

--- src/main/java/Person.java
+++ src/main/java/Person.java
@@ -1,10 +1,14 @@
 public class Person (){
+   
+   // name of the person
    private String name;
    
+   // getter for name
    public String getName(){
        return this.name;
    }
    
+   // setter for name
    public void setName(String name){
        this.name = name;
    } 
